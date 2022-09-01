37f329b31ef90ed82296c157339d746f5a273404
Philip Hadviger <613305+datfinesoul@users.noreply.github.com>
2022-09-01 16:13:48 +0900

    refactor to make user experience better

--- version.tf
+++ version.tf
@@ -1,35 +1,22 @@
 ### OVERVIEW BEGIN
-# Please preserve this section for easier merges from the
-# template repo.
-#
-# Lower Bounds:
-#   Providers:
-#     Focus on the target audience. For modules, compatibility is
-#     more important that being completely up to date.  When writing a
-#     module, it makes sense to really find a good middle ground for
-#     provider lower bounds.
-#   Terraform:
-#     Similar to the above, support as much as possible here, so the
-#     module can actually get good use.  It's really up to the consumer
-#     to define these constraints more than the module, but set a good
-#     lower bound that makes the module safe.  This is where the matrix
-#     in the GitHub action will help you.
-#
-# Upper Bounds:
-#   For modules, it's highly recommended not to set an uppper bound
-#   for versions, as it leads to situations where a module can no
-#   longer be used, because the upper bound unnecessarily forces the
-#   module to be upgraded, even though it would still function in higher
-#   versions.
-#
-# Make a sensible choice here based on what you plan on supporting.
+# Refer to the 'Module Versioning' section in the README for more information
 ### OVERVIEW END
 terraform {
   required_version = ">= 1.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.74"
+      version = ">= 4.27"
+
+      # The template recommendation is to write your module with a provider
+      #   that is supplied when the module is instantiated, vs using the
+      #   default provider.
+      # If you really need to use the default provider only, then please
+      #   the configuration_aliases below, and from anything in the examples/
+      #   directory.
+      configuration_aliases = [
+        aws.primary,
+      ]
     }
   }
 }
